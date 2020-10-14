960d72aa74db24fffcd9cd52dd7cc723c68415fb
unknown <km123abca>
2020-10-14 22:26:14 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Facebook from "./Facebook/Facebook";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/fb">
+            <Facebook />
+          </Route>
+          <Route path="/">
+            <h1>Hello, there is nothing here</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Facebook/Facebook.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Facebook() {
+  return (
+    <div>
+      <h1>This is facebook</h1>
+    </div>
+  );
+}
+
+export default Facebook;
